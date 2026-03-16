89ce7d269ccc18aac4d60ffb12e0ec1d1a30bea2
Bárbara Marques <barbaralimamarques3935@gmail.com>
2026-03-16 10:24:48 -0300

    Projeto dev-quiz

---
+++ src/questions.js
@@ -0,0 +1,56 @@
+export const questions = [
+  {
+    question: "O que significa HTML?",
+    options: [
+      "Hyper Transfer Markup Language",
+      "Hyper Text Markup Language",
+      "High Text Machine Language",
+      "Hyper Tool Multi Language"
+    ],
+    answer: 1
+  },
+
+  {
+    question: "Qual linguagem roda no navegador?",
+    options: [
+      "Python",
+      "Java",
+      "JavaScript",
+      "C++"
+    ],
+    answer: 2
+  },
+
+  {
+    question: "O que é Git?",
+    options: [
+      "Linguagem de programação",
+      "Sistema de controle de versão",
+      "Banco de dados",
+      "Servidor web"
+    ],
+    answer: 1
+  },
+
+  {
+    question: "Qual tecnologia estiliza páginas web?",
+    options: [
+      "CSS",
+      "Node.js",
+      "Docker",
+      "SQL"
+    ],
+    answer: 0
+  },
+
+  {
+    question: "O que significa API?",
+    options: [
+      "Application Programming Interface",
+      "Advanced Program Internet",
+      "Application Process Integration",
+      "Automated Program Interface"
+    ],
+    answer: 0
+  }
+]
